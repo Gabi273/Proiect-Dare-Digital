c4cedb14eae522f60ff53891bd9129defa74687b
Gabi273 <96780484+Gabi273@users.noreply.github.com>
2022-07-30 14:48:52 +0300

    Add files via upload

--- !proiect Dare DIgital/script.js
+++ !proiect Dare DIgital/script.js
@@ -8,58 +8,27 @@ button.addEventListener("click",() =>{
 
 /*SLIDER*/
 
-const slides = document.querySelectorAll('.slide');
-const next = document.getElementById('next');
-const prev = document.getElementById('prev');
-const first = document.getElementById('first');
-const auto = true;
-
-
-const nextSlide = () => {
-    //get current
-    const current = document.querySelector('.current');
-    //remove current
-    current.classList.remove('.current');
-    //check for next slide
-    if(current.nextElementSibling){
-        //add current to next sibling
-        current.nextElementSibling.classList.add('current');
-    } else {
-        //add current to start
-        slides[0].classList.add('current');
-    }
-    setTimeout(() => current.classList.remove('current'));
+function initOwl(){
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 30,
+        nav: true,
+        dots: true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:4
+            }
+        }
+    }) 
 }
 
-const prevSlide = () => {
-    //get current
-    const current = document.querySelector('.current');
-    //remove current
-    current.classList.remove('.current');
-    //check for prev slide
-    if(current.previousElementSibling){
-        //add current to prev sibling
-        current.previousElementSibling.classList.add('current');
-    } else {
-        //add current to last
-        slides[slides.length - 1].classList.add('current');
-    }
-    setTimeout(() => current.classList.remove('current'));
-}
+window.addEventListener('load', function () {
+    initOwl();
+} )
 
-//pentru a face automat
-// const interval = 7500;
-let slideInterval;
-
-
-next.addEventListener('click', e => {
-    nextSlide();
-});
-prev.addEventListener('click', e => {  
-    prevSlide();
-});
-//pentru a face automat
-// if (auto) {
-//     // Run next slide at interval time
-//     slideInterval = setInterval(nextSlide, interval);
-//   } 
