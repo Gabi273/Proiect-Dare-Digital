7e04476676777b1a6fe647841a26a6867e4eb6c5
Gabi273 <96780484+Gabi273@users.noreply.github.com>
2022-08-15 06:46:55 +0300

    Add files via upload

--- !proiect Dare DIgital/script.js
+++ !proiect Dare DIgital/script.js
@@ -6,6 +6,34 @@ button.addEventListener("click",() =>{
     navbar.classList.toggle("open");
 })
 
+// const buttonSection1 = document.getElementById('button-section-1');
+// const section1 = document.querySelector('.section-1');
+
+// buttonSection1.addEventListener('click', () =>{
+//     section1.classList.toggle("open");
+// });
+
+// const buttonSection2 = document.getElementById('button-section-2');
+// const section2 = document.querySelector('.section-2');
+
+// buttonSection2.addEventListener('click', () =>{
+//     section2.classList.toggle("open");
+// });
+
+// const buttonSection3 = document.getElementById('button-section-3');
+// const section3 = document.querySelector('.section-3');
+
+// buttonSection3.addEventListener('click', () =>{
+//     section3.classList.toggle("open");
+// });
+
+// const buttonSection4 = document.getElementById('button-section-4');
+// const section4 = document.querySelector('.section-4');
+
+// buttonSection4.addEventListener('click', () =>{
+//     section4.classList.toggle("open");
+// });
+
 /*SLIDER*/
 
 function initOwl(){
@@ -38,3 +66,45 @@ window.addEventListener('load', function () {
     initOwl();
 } )
 
+/* DRAGGEr*/
+
+const dragger = document.getElementById('dragger');
+const fileSelectorInput = document.getElementById('fileSelectorInput');
+
+fileSelectorInput.addEventListener('change', function(){
+    file = this.files[0]
+    uploadFileHandler(file);
+});
+
+dragger.addEventListener('dragover', (e) =>{
+    e.preventDefault();
+
+});
+
+dragger.addEventListener('dragleave', () =>{
+
+});
+
+dragger.addEventListener('drop', (e) =>{
+    e.preventDefault();
+    file = e.dataTransfer.files[0]
+    uploadFileHandler(file);
+});
+
+const uploadFileHandler = (file) => {
+    const validFileExtensions = ['image/jpeg', 'image/jpg', 'image/png'];
+    
+    if(validFileExtensions.includes(file.type)){
+        
+        const fileReader = new FileReader();
+        
+        fileReader.onload = () =>{
+            const fileURL = fileReader.result;
+            const imageTag = `<img src=${fileURL} alt="" />`
+            dragger.innerHTML = imageTag;
+        }
+
+        fileReader.readAsDataURL(file);
+    }
+
+}
